0ab51179a2c8121e5916a9cd27bd22be9d0d5d29
js-diaz <js.diaz@uniandes.edu.co>
2018-09-16 19:11:24 -0500

    error en index

--- app.js
+++ app.js
@@ -4,14 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const robosRouter = require('./routes/robos');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'front/build')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/robos', robosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
